5a641395fb336313f091d28ffb17625df836d2a3
Ângelo Nuffer <angelonuffer@gmail.com>
2015-05-17 10:39:58 +0000

    Create script that helps to create an admin user.

---
+++ create_admin.py
@@ -0,0 +1,13 @@
+from getpass import getpass
+from md5 import md5
+
+from redis import Redis
+
+def main():
+    database = Redis()
+    database.set("user:admin:password", md5(getpass()).hexdigest())
+    database.delete("user:admin:profiles")
+    database.rpush("user:admin:profiles", "admin")
+
+if __name__ == "__main__":
+    main()
