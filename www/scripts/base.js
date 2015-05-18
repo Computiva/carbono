da4c47b7423146dcc46584f0fced5c63b74f4a02
Ângelo Nuffer <angelonuffer@gmail.com>
2015-05-18 21:12:01 +0000

    Change icons to font-awesome.

--- www/scripts/base.js
+++ www/scripts/base.js
@@ -1,17 +1,5 @@
 $(document).ready(function() {
-    $("input[type=submit], a.button").button();
-    $("a#home").button({
-        icons: {
-            primary: "ui-icon-home",
-        },
-        text: false,
-    });
-    $("a.remove").button({
-        icons: {
-            primary: "ui-icon-trash",
-        },
-        text: false,
-    });
+    $("input[type=submit]").button();
     $("span.list_item").buttonset();
     $("div.checkboxes").buttonset();
 });
