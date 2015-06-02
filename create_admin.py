a3df6725a04e0caccf85d8c53a88825ea950c04a
alzemand <edilson_alz@hotmail.com>
2015-06-02 09:39:47 -0300

    Home icons and designer adjust

--- create_admin.py
+++ create_admin.py
@@ -4,6 +4,7 @@ from md5 import md5
 from redis import Redis
 
 def main():
+    print("Enter you password to create a root user")
     database = Redis()
     database.set("user:admin:password", md5(getpass()).hexdigest())
     database.delete("user:admin:profiles")
